ac5de7d355069eff05add5777831ee4574b2d549
Arun Nair <davor.suker09@gmail.com>
2023-02-12 20:28:24 -0600

    navigation 1

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,28 +1,35 @@
-import { FlatList, Text } from "react-native";
-import { Avatar, ListItem } from "react-native-elements";
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { Avatar, ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
-const DirectoryScreen = (props) => {
-  const renderDirectoryItem = ({ item: campsite }) => {
+const DirectoryScreen = ({ navigation }) => {
+    const [campsites, setCampsites] = useState(CAMPSITES);
+
+    const renderDirectoryItem = ({ item: campsite }) => {
+        return (
+            <ListItem
+                onPress={() =>
+                    navigation.navigate('CampsiteInfo', { campsite })
+                }
+            >
+                <Avatar source={campsite.image} rounded />
+                <ListItem.Content>
+                    <ListItem.Title>{campsite.name}</ListItem.Title>
+                    <ListItem.Subtitle>
+                        {campsite.description}
+                    </ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        );
+    };
     return (
-      <ListItem onPress={() => props.onPress(campsite.id)}>
-        <Avatar
-          source={campsite.image}
-          rounded
+        <FlatList
+            data={campsites}
+            renderItem={renderDirectoryItem}
+            keyExtractor={(item) => item.id.toString()}
         />
-        <ListItem.Content>
-          <ListItem.Title>{campsite.name}</ListItem.Title>
-          <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
     );
-  };
-  return (
-    <FlatList
-      data={props.campsites}
-      renderItem={renderDirectoryItem}
-      keyExtractor={(item) => item.id.toString()}
-    />
-  );
 };
 
 export default DirectoryScreen;
