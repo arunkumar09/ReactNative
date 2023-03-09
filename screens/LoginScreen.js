613d3a08927fd15ce781bf9357900ca858733b26
Arun Nair <davor.suker09@gmail.com>
2023-03-09 17:26:29 -0600

    Picking an Image

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,103 +1,106 @@
 import { useEffect, useState } from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView , Image} from 'react-native';
 import { CheckBox, Input, Button, Icon } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as ImagePicker from "expo-image-picker";
+import { baseUrl } from "../shared/baseUrl";
+import logo from "../assets/images/logo.png";
 
 const LoginTab = ({ navigation }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [remember, setRemember] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
 
-    const handleLogin = () => {
-        console.log('username:', username);
-        console.log('password:', password);
-        console.log('remember:', remember);
-        if (remember) {
-            SecureStore.setItemAsync(
-                'userinfo',
-                JSON.stringify({
-                    username,
-                    password
-                })
-            ).catch((error) => console.log('Could not save user info', error));
-        } else {
-            SecureStore.deleteItemAsync('userinfo').catch((error) =>
-                console.log('Could not delete user info', error)
-            );
-        }
-    };
+  const handleLogin = () => {
+    console.log("username:", username);
+    console.log("password:", password);
+    console.log("remember:", remember);
+    if (remember) {
+      SecureStore.setItemAsync(
+        "userinfo",
+        JSON.stringify({
+          username,
+          password,
+        })
+      ).catch((error) => console.log("Could not save user info", error));
+    } else {
+      SecureStore.deleteItemAsync("userinfo").catch((error) =>
+        console.log("Could not delete user info", error)
+      );
+    }
+  };
 
-    useEffect(() => {
-        SecureStore.getItemAsync('userinfo').then((userdata) => {
-            const userinfo = JSON.parse(userdata);
-            if (userinfo) {
-                setUsername(userinfo.username);
-                setPassword(userinfo.password);
-                setRemember(true);
-            }
-        });
-    }, []);
+  useEffect(() => {
+    SecureStore.getItemAsync("userinfo").then((userdata) => {
+      const userinfo = JSON.parse(userdata);
+      if (userinfo) {
+        setUsername(userinfo.username);
+        setPassword(userinfo.password);
+        setRemember(true);
+      }
+    });
+  }, []);
 
-    return (
-        <View style={styles.container}>
-            <Input
-                placeholder='Username'
-                leftIcon={{ type: 'font-awesome', name: 'user-o' }}
-                onChangeText={(text) => setUsername(text)}
-                value={username}
-                containerStyle={styles.formInput}
-                leftIconContainerStyle={styles.formIcon}
+  return (
+    <View style={styles.container}>
+      <Input
+        placeholder="Username"
+        leftIcon={{ type: "font-awesome", name: "user-o" }}
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+        containerStyle={styles.formInput}
+        leftIconContainerStyle={styles.formIcon}
+      />
+      <Input
+        placeholder="Password"
+        leftIcon={{ type: "font-awesome", name: "key" }}
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        containerStyle={styles.formInput}
+        leftIconContainerStyle={styles.formIcon}
+      />
+      <CheckBox
+        title="Remember Me"
+        center
+        checked={remember}
+        onPress={() => setRemember(!remember)}
+        containerStyle={styles.formCheckbox}
+      />
+      <View style={styles.formButton}>
+        <Button
+          onPress={() => handleLogin()}
+          title="Login"
+          color="#5637DD"
+          icon={
+            <Icon
+              name="sign-in"
+              type="font-awesome"
+              color="#fff"
+              iconStyle={{ marginRight: 10 }}
             />
-            <Input
-                placeholder='Password'
-                leftIcon={{ type: 'font-awesome', name: 'key' }}
-                onChangeText={(text) => setPassword(text)}
-                value={password}
-                containerStyle={styles.formInput}
-                leftIconContainerStyle={styles.formIcon}
+          }
+          buttonStyle={{ backgroundColor: "#5637DD" }}
+        />
+      </View>
+      <View style={styles.formButton}>
+        <Button
+          onPress={() => navigation.navigate("Register")}
+          title="Register"
+          type="clear"
+          icon={
+            <Icon
+              name="user-plus"
+              type="font-awesome"
+              color="blue"
+              iconStyle={{ marginRight: 10 }}
             />
-            <CheckBox
-                title='Remember Me'
-                center
-                checked={remember}
-                onPress={() => setRemember(!remember)}
-                containerStyle={styles.formCheckbox}
-            />
-            <View style={styles.formButton}>
-                <Button
-                    onPress={() => handleLogin()}
-                    title='Login'
-                    color='#5637DD'
-                    icon={
-                        <Icon
-                            name='sign-in'
-                            type='font-awesome'
-                            color='#fff'
-                            iconStyle={{ marginRight: 10 }}
-                        />
-                    }
-                    buttonStyle={{ backgroundColor: '#5637DD' }}
-                />
-            </View>
-            <View style={styles.formButton}>
-                <Button
-                    onPress={() => navigation.navigate('Register')}
-                    title='Register'
-                    type='clear'
-                    icon={
-                        <Icon
-                            name='user-plus'
-                            type='font-awesome'
-                            color='blue'
-                            iconStyle={{ marginRight: 10 }}
-                        />
-                    }
-                    titleStyle={{ color: 'blue' }}
-                />
-            </View>
-        </View>
-    );
+          }
+          titleStyle={{ color: "blue" }}
+        />
+      </View>
+    </View>
+  );
 };
 
 const RegisterTab = () => {
@@ -107,6 +110,7 @@ const RegisterTab = () => {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [remember, setRemember] = useState(false);
+  const [imageUrl, setImageUrl] = useState(baseUrl + "images/logo.png");
 
   const handleRegister = () => {
     const userInfo = {
@@ -134,9 +138,35 @@ const RegisterTab = () => {
     }
   };
 
+  const getImageFromCamera = async () => {
+    const cameraPermission = await ImagePicker.requestCameraPermissionsAsync();
+    if (cameraPermission.status === "granted") {
+      const capturedImage = await ImagePicker.launchCameraAsync();
+      ({
+        allowsEditing: true,
+        aspect: [1, 1],
+      });
+      if (capturedImage.assets) {
+        console.log(capturedImage.assets[0]);
+        setImageUrl(capturedImage.assets[0].uri);
+      }
+    }
+  };
+
   return (
     <ScrollView>
       <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={{ uri: imageUrl }}
+            loadingIndicatorSource={logo}
+            style={styles.image}
+          />
+          <Button
+            title="Camera"
+            onPress={getImageFromCamera}
+          />
+        </View>
         <Input
           placeholder="Username"
           leftIcon={{ type: "font-awesome", name: "user-o" }}
@@ -272,6 +302,17 @@ const styles = StyleSheet.create({
     margin: 20,
     marginRight: 40,
     marginLeft: 40,
+  },
+  imageContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    margin: 10,
+  },
+  image: {
+    width: 60,
+    height: 60,
   },
 });
 
